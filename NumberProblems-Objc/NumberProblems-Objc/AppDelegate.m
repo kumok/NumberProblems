ef31f45e348e4b69da5bdbeda2d1486e3b6c4da9
kumok <bhh@byu.edu>
2014-12-28 18:24:45 -0700

    final

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -18,9 +18,45 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
-    
+    iterateCount(4);
     
     return YES;
 }
 
+int divisor = 6;
+int multiplie = 8;
+double divided1 (int myNumber);
+int multiplied (int myNumber);
+
+int iterateCount (int myNumber)
+{
+    if (myNumber > 0)
+    {
+        NSLog(@"Iterate %d", myNumber);
+        int result = multiplied(myNumber);
+        NSLog(@"Multiplied %d by %d to be %d", myNumber, multiplie, result);
+        
+        double result2;
+        result2 = divided1(result);
+        NSLog(@"Divided %d by %d to be %f", result, divisor , result2);
+        
+        myNumber--;
+        iterateCount(myNumber);
+    }
+    return 0;
+}
+
+
+
+int multiplied (int myNumber)
+{
+        return myNumber * multiplie;
+}
+
 @end
+
+
+double divided1 (int myNumber)
+{
+    return (myNumber / (float)divisor);
+}
